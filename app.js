9784cf2307f89a3d57fc21dc2b103ad000a06a91
arbabnasru <148573131+arbabnasrullahkhan@users.noreply.github.com>
2026-07-24 22:17:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -164,14 +164,7 @@ function initIslandDock() {
         <svg viewBox="0 0 24 24"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
         <span>Home</span>
       </a>
-      <a href="order.html" class="nav-item ${currentPage === 'order.html' ? 'active' : ''}">
-        <svg viewBox="0 0 24 24"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
-        <span>Order</span>
-      </a>
-      <a href="https://t.me/RENTAL_DOLLOR" target="_blank" class="nav-item">
-        <svg viewBox="0 0 24 24"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>
-        <span>Support</span>
-      </a>
+
       <a href="profile.html" class="nav-item ${currentPage === 'profile.html' ? 'active' : ''}">
         ${profileIconHtml}
         <span>Profile</span>
